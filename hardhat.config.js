5717c4e020e0c787be98426d505487ca7952c9c1
cocolasagna <msujan795@gmail.com>
2023-12-30 17:09:00 +0545

    small changes: fdf

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,9 +5,6 @@ module.exports = {
   solidity: "0.8.19",
   networks: {
     hardhat: {
-      forking:{
-        url:'https://eth-mainnet.g.alchemy.com/v2/JXEK-pErvSiCUG-xy0wtDlCOxlTwM2OM'
-      }
      
     },
   },
